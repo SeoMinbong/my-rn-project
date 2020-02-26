a0e6fbe5fc40c83fbd6c0f8e07e2964b7cf0543a
Bong <smb0209@nate.com>
2020-02-26 14:40:18 +0900

    2nd commit props, map, loop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
-//실질적인 화면에 출력하는 역할
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://naver.com"
-          rel="hihihi"
-        >
-          네이버로 가기
-        </a>
-      </header>
-    </div>
-  );
+
+const customers = [{
+  'id':1,
+  'image':'https://placeimg.com/150/100/1',
+  'name':"David",
+  'birthday':"910215",
+  'gender':"male",
+  'job':"Dev"
+},{
+  'id':2,
+  'image':'https://placeimg.com/150/100/2',
+  'name':"Jung",
+  'birthday':"910210",
+  'gender':"female",
+  'job':"Design"
+},{
+  'id':3,
+  'image':'https://placeimg.com/150/100/3',
+  'name':"Solomon",
+  'birthday':"920215",
+  'gender':"male",
+  'job':"Dev"
+}];
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {customers.map(c=>{
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
